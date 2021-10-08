87fa1b22e732416e8cf1673780d1b29857bf5243
oskardelgado84 <90490551+oskardelgado84@users.noreply.github.com>
2021-10-07 23:19:48 -0500

    Modulo de ReportesUsuarios.JS

--- PetShop/src/main/webapp/js/reportesUsuarios.js
+++ PetShop/src/main/webapp/js/reportesUsuarios.js
@@ -1,3 +1,40 @@
-/**
- * 
- */
+
+$(document).ready(function(){
+	
+	function listarUsuario(){
+		
+	$.ajax({
+	type:"post",
+	url:"ReportesUsuarios",
+	dataType:"json",
+	data:{opcion:"Usuarios"},
+		success:function(result){
+		console.log(result);
+		var tabla=document.querySelector("#tabla")
+		tabla.innerHTML=''
+		tabla.innerHTML+=`<tr>
+		<th class='th1'>Cedula</th>
+		<th class='th1'>Nombre</th>
+		<th class='th1'>Correo Electronico</th>
+		<th class='th1'>Usuario</th>
+		<th class='th1'>Password</th>
+		</tr>`
+		for(let user of result){
+		tabla.innerHTML+=`<tr>
+		<td class='td1'>${user.cedula_usuario}</td>
+		<td class='td1'>${user.nombre_usuario}</td>
+		<td class='td1'>${user.email_usuario}</td>
+		<td class='td1'>${user.usuario}</td>
+		<td class='td1'>${user.password}</td>
+		</tr>`
+		}
+		}
+		});	
+	}
+listarUsuario();
+/*
+$('.listaUsuarios').on('click',function(){
+	listarUsuario()
+});
+*/
+})
